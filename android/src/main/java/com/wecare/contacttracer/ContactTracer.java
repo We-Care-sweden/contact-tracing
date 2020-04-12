fc4607e6f41d0bc8e4ef5fe3e367983272377ea6
NiekBijman <niekbijman@gmail.com>
2020-04-12 18:08:45 +0200

    Add methods described in Google/Apple contact tracing specification which uses Google's Task API

--- android/src/main/java/com/wecare/contacttracer/ContactTracer.java
+++ android/src/main/java/com/wecare/contacttracer/ContactTracer.java
@@ -7,6 +7,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
+import com.google.android.gms.tasks.Task;
 
 import java.util.List;
 
@@ -40,7 +41,87 @@ public class ContactTracer extends ReactContextBaseJavaModule implements Contact
      * Callers need to re-invoke this after each device restart, providing a new
      * callback PendingIntent.
      */
-    //Task<Status> startContactTracing(PendingIntent contactTracingCallback);
+    Task<Status> startContactTracing(PendingIntent contactTracingCallback) {
+        return null;
+    }
+
+    /**
+     * Disables advertising and scanning related to contact tracing. Contents of the
+     * database and keys will remain.
+     *
+     * If the client app has been uninstalled by the user, this will be automatically
+     * invoked and the database and keys will be wiped from the device.
+     */
+    Task<Status> stopContactTracing() {
+        return null;
+    }
+
+    /**
+     * Indicates whether contact tracing is currently running for the
+     * requesting app.
+     */
+    Task<Status> isContactTracingEnabled() {
+        return null;
+    }
+
+    /**
+     * Flags daily tracing keys as to be stored on the server.
+     *
+     * This should only be done after proper verification is performed on the
+     * client side that the user is diagnosed positive.
+     *
+     * Calling this will invoke the
+     * ContactTracingCallback.requestUploadDailyTracingKeys callback
+     * provided via startContactTracing at some point in the future. Provided keys
+     * should be uploaded to the server and distributed to other users.
+     *
+     * This shows a user dialog for sharing and uploading data to the server.
+     * The status will also flip back off again after 14 days; in other words,
+     * the client will stop receiving requestUploadDailyTracingKeys
+     * callbacks after that time.*
+     * * Only 14 days of history are available.
+     **/
+    Task<Status> startSharingDailyTracingKeys() {
+        return null;
+    }
+
+    /**
+     * Provides a list of diagnosis keys for contact checking. The keys are to be
+     * provided by a centralized service (e.g. synced from the server).
+     *
+     * When invoked after the requestProvideDiagnosisKeys callback, this triggers a
+     * recalculation of contact status which can be obtained via hasContact()
+     * after the calculation has finished.
+     *
+     * Should be called with a maximum of N keys at a time.
+     */
+    Task<Status> provideDiagnosisKeys(List<DailyTracingKey> keys) {
+        return null;
+    }
+
+    /**
+     * The maximum number of keys to pass into provideDiagnosisKeys at any given
+     * time.
+     */
+    int getMaxDiagnosisKeys() {
+        return -1;
+    }
+
+    /**
+     * Check if this user has come into contact with a provided key. Contact
+     * calculation happens daily.
+     */
+    Task<Boolean> hasContact() {
+        return null;
+    }
+
+    /**
+     * Check if this user has come into contact with a provided key. Contact
+     * calculation happens daily.
+     */
+    Task<List<ContactInfo>> getContactInformation() {
+        return null;
+    }
 
 
     @Override
